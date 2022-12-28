2fdc4bfec39937320c7b6b2ccd0c395cb011e873
Ilya Nazarchuk <45705923+GoSEHawks@users.noreply.github.com>
2022-12-28 14:30:02 -0600

    Update streamlit_app.py
    added a few menu items  to the main menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My new Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spanish & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
