96c7d587e197ed130ef667d4dd06aca86c777e99
Ilya Nazarchuk <45705923+GoSEHawks@users.noreply.github.com>
2022-12-29 13:45:06 -0600

    Update streamlit_app.py
    bit of clean up, and confirmed that we didn't break anything

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
+
 
 streamlit.title('My new Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#import pandas 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,7 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 # normalizes the json data
@@ -32,7 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs the normalized data to a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -43,5 +50,11 @@ streamlit.dataframe(my_data_rows)
 #second fruit entry row
 fruit_choices = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', fruit_choices)
+fruityvice_responses = requests.get("https://fruityvice.com/api/fruit/" +fruit_choices)
+# normalizes the json data
+fruityvice_normalized1 = pandas.json_normalize(fruityvice_responses.json())
+# outputs the normalized data to a table
+streamlit.dataframe(fruityvice_normalized1)
+#end of second entry row
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
